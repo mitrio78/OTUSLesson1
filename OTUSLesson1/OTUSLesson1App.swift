2bcd12552b9aca35235d5b5f6c2c08935ac5e17f
Grishechko, Dmitriy <ext_dgrishechko@rencredit.ru>
2023-06-02 18:48:35 +0300

    LS1: init commit

---
+++ OTUSLesson1/OTUSLesson1App.swift
@@ -0,0 +1,17 @@
+//
+//  OTUSLesson1App.swift
+//  OTUSLesson1
+//
+//  Created by anduser on 02.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct OTUSLesson1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
